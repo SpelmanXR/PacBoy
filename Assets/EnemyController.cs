340c7587796de8cf921377909e687912f5a6b849
jvolcy <jerry.volcy@gmail.com>
2022-08-15 19:11:01 -0400

    Attributions Notes.txt file added

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float Speed = 1f;
-    float speed;
+    float directionalSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = Speed;
+        directionalSpeed = Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime * Vector2.right);
+        transform.Translate(directionalSpeed * Time.deltaTime * Vector2.right);
     }
 
     private void OnTriggerEnter2D(Collider2D otherObject)
     {
-        if (otherObject.name == "LeftWall") speed = Speed;
-        else if (otherObject.name == "RightWall") speed = -Speed;
+        if (otherObject.name == "LeftWall") directionalSpeed = Speed;
+        else if (otherObject.name == "RightWall") directionalSpeed = -Speed;
     }
 }
